2df6bbfec5112201a9ff77f92a141c065bc5ca61
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-06-11 21:17:55 +0300

    commit

--- src/components/Home/Layout.jsx
+++ src/components/Home/Layout.jsx
@@ -6,7 +6,7 @@ import Event from './Event'
 
 const Layout = () => {
     return (
-        <div style={{ margin: '0 auto', padding: '0 1rem' }} className="w-full max-w-[1241px] min-h-screen px-4 md:px-6 py-10 md:py-16 flex flex-col items-center bg-white">
+        <div style={{ margin: '0 auto', marginTop: '2rem', padding: '0 1rem' }} className="w-full max-w-[1241px] min-h-screen px-4 md:px-6 py-10 md:py-16 flex flex-col items-center bg-white">
             <Hero />
             <Sample />
             <Event />
